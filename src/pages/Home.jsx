c168d06e4deb3f53ae36936be83c6ba7cb862646
unknown <nuno_fernandes18@hotmail.com>
2021-03-05 14:34:49 +0000

    05.03.2021

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,10 +14,11 @@ const Home = () => {
                 <Navbar />
 
                 <div className="content-background">
-                    <video autoPlay playsInline muted loop>
+                    <video src={videoBackground} autoPlay playsInline muted loop></video>
+                    {/* <video autoPlay playsInline muted loop>
 
                         <source src={videoBackground} type="video/mp4"></source>
-                    </video>
+                    </video> */}
 
                     <div className="container">
                         <img src={logo}></img>
